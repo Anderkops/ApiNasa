532152adb722bbc89dd0e488f86c8b5d0577d3ed
Anderkops <and_first@yahoo.com.br>
2017-10-12 23:46:56 -0300

    Criação do projeto

---
+++ DadosNasa/ImagemNasa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DadosNasa
+{
+    public class ImagemNasa
+    {
+        public DateTime Data { get; set; }
+
+        public string Titulo { get; set; }
+
+        public string Descricao { get; set; }
+
+        public string URL { get; set; }
+
+        public string MediaType { get; set; }
+    }
+}
